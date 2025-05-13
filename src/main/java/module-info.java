517731b23903ea42d1f88ea53ec24f371d7933be
Suriok <150962560+Suriok@users.noreply.github.com>
2025-05-13 23:44:29 +0200

    Correct module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,14 +2,15 @@ module cz.cvut.fel.pjv.golyakat.dungeon_escape {
     requires java.desktop;
     requires java.logging;
 
-    requires com.fasterxml.jackson.annotation;
+
     requires com.fasterxml.jackson.databind;
+    requires com.fasterxml.jackson.core;
+    requires com.fasterxml.jackson.annotation;
     requires com.fasterxml.jackson.dataformat.xml;
 
     exports cz.cvut.fel.pjv.golyakat.dungeon_escape;
 
-    opens  cz.cvut.fel.pjv.golyakat.dungeon_escape
-            to javafx.fxml,
-            com.fasterxml.jackson.databind,
-            com.fasterxml.jackson.dataformat.xml;
+    opens cz.cvut.fel.pjv.golyakat.dungeon_escape
+            to com.fasterxml.jackson.databind, com.fasterxml.jackson.dataformat.xml;
+
 }
